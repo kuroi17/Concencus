1cdb9bcd2c455d14440a376936c47741c36c8674
kuroi017 <painkira518@gmail.com>
2026-04-23 18:39:32 +0800

    fix

--- frontend/src/components/ChatComponents/ConversationListPanel.jsx
+++ frontend/src/components/ChatComponents/ConversationListPanel.jsx
@@ -227,7 +227,6 @@ function ConversationListPanel({
                     </div>
                   </button>
                 );
-                );
               })}
             </div>
           </>
